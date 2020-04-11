e009cd22469de4dc017435702c8beb3768d20118
nawaf <princenawaf99@hotmail.com>
2020-04-11 03:17:22 +0300

    message

---
+++ app_script.js
@@ -0,0 +1,175 @@
+/*
+game mood if it is false  it is difficult and if it is true it is easy so based on that we will decide how many squares we need to do ;;
+
+*/
+let gameMood = false;
+let squares = document.querySelectorAll('.square');
+let target = document.querySelector("#target");
+let h1 = document.querySelector('h1');
+let message = document.querySelector('#message');
+let controller = document.querySelector('#controller');
+let picked_color = null;
+
+let easyMode = document.querySelector('#easy');
+let difficultMode = document.querySelector('#difficult');
+// i will do an event for each them to determine the mode of the game ;;
+easyMode.addEventListener('click', function () {
+    gameMood = true;
+    difficultMode.classList.remove('selected');
+    easyMode.classList.add('selected');
+    startGame();
+
+});
+difficultMode.addEventListener('click', function () {
+    //other wise by default it will be in difficult mode ;;
+    gameMood = false;
+    easyMode.classList.remove('selected');
+    difficultMode.classList.add('selected');
+    startGame();
+});
+
+startGame();
+
+/*
+code for starting the game ;;
+for (let k = 0; k < squares.length; k++) {
+
+    squares[k].style.backgroundColor = list[k];
+    squares[k].addEventListener('click', function () {
+        if (this.style.backgroundColor != picked_color) {
+            this.style.backgroundColor = '#232323';
+            message.textContent = 'try again!';
+        } else {
+            alterAllSquares(this.style.backgroundColor);
+            message.textContent = 'correct!';
+
+        }
+
+
+    });
+}*/
+
+// picking the random color then display it
+
+
+function alterAllSquares(rgb, num) {
+    for (let k = 0; k < num; k++) {
+        squares[k].style.backgroundColor = rgb;
+    }
+    h1.style.backgroundColor = rgb;
+    controller.textContent = 'New Game?';
+
+}
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function generateRandomColors(num) {
+    let list = [];
+    for (let k = 0; k < num; k++) {
+        list.push(generateRgb());
+    }
+    return list;
+
+}
+
+function generateRgb() {
+    let r = getRandomInt(0, 255);
+    let g = getRandomInt(0, 255);
+    let b = getRandomInt(0, 255);
+    return 'rgb(' + r + ', ' + g + ', ' + b + ')'
+
+}
+
+// by default it will start the game in the hard mode ;;
+function startGame() {
+    h1.style.backgroundColor = '#232323';
+    message.textContent = '';
+    let list = null;
+    let random_picked_color = 0;
+    if (gameMood === false) {
+        list = generateRandomColors(6);
+        random_picked_color = getRandomInt(0, 5);
+        picked_color = list[random_picked_color];
+    } else if (gameMood === true) {
+        list = generateRandomColors(3);
+        random_picked_color = getRandomInt(0, 2);
+        picked_color = list[random_picked_color];
+        clear_last_three_squares();
+    }
+    for (let k = 0; k < list.length; k++) {
+
+        squares[k].style.backgroundColor = list[k];
+        squares[k].addEventListener('click', function () {
+            console.log(this.style.backgroundColor + "                " + picked_color);
+            if (this.style.backgroundColor != picked_color) {
+                this.style.backgroundColor = '#232323';
+                message.textContent = 'try again!';
+            } else {
+                if (gameMood === false)
+                    alterAllSquares(this.style.backgroundColor, 6);
+                else if (gameMood === true)
+                    alterAllSquares(this.style.backgroundColor, 3);
+                message.textContent = 'correct!';
+
+            }
+
+
+        });
+
+    }
+    squares[random_picked_color].style.backgroundColor;
+    target.textContent = picked_color;
+}
+
+controller.addEventListener('click', function () {
+    // if the user did not like the colors he can change the colors by starting new game ;;
+    controller.textContent = 'New Colors?';
+    startGame();
+
+});
+
+
+/*
+function startEasy() {
+    clear_last_three_squares();
+    h1.style.backgroundColor = '#232323';
+    message.textContent = '';
+    let list = generateRandomColors(3);
+    let random_picked_color = getRandomInt(0, 2);
+    picked_color = list[random_picked_color];
+    for (let k = 0; k < list.length; k++) {
+
+        squares[k].style.backgroundColor = list[k];
+        squares[k].addEventListener('click', function () {
+            console.log(this.style.backgroundColor + "                " + picked_color);
+            if (this.style.backgroundColor != picked_color) {
+                this.style.backgroundColor = '#232323';
+                message.textContent = 'try again!';
+            } else {
+                alterAllSquares(this.style.backgroundColor, 3);
+                message.textContent = 'correct!';
+
+            }
+
+
+        });
+
+    }
+    squares[random_picked_color].style.backgroundColor;
+    target.textContent = picked_color;
+}*/
+
+function clear_last_three_squares() {
+    for (let k = 3; k < 6; k++) {
+        squares[k].style.backgroundColor = ' #232323';
+    }
+
+}
+
+
+
+
